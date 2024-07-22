159b78d08d93ea127dedf241fd49e3ddffe8c79e
Max Normand <maxnormand97@gmail.com>
2024-07-22 14:38:22 +0100

    fix bug in db call to ENV

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,2 @@
 const mongoose = require('mongoose')
-// mongoose.connect(process.env.MONGODB_URL);
-const uri = `mongodb://localhost:27017/${process.env.NODE_ENV}`
-mongoose.connect(process.env.MONGODB_URI || uri);
+mongoose.connect(process.env.MONGODB_URL);
