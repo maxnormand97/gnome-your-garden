ec22d340edd614d1382cfa1a0f226d2e08b9e34c
Max Normand <maxnormand97@gmail.com>
2024-07-22 14:28:07 +0100

    fix package.json dev script - update how envs are called to make it better

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL);
+// mongoose.connect(process.env.MONGODB_URL);
+const uri = `mongodb://localhost:27017/${process.env.NODE_ENV}`
+mongoose.connect(process.env.MONGODB_URI || uri);
--- server/index.js
+++ server/index.js
@@ -1,21 +1,8 @@
-// TODO: this will have to change based on the ENV right?
-require('dotenv').config({ path: './.env.development' });
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 8000;
-require('./db/mongoose')
-const plantRouter = require('./routers/plants')
-const userRouter = require('./routers/users')
-app.use(express.json());
-app.use(cors())
-app.use(userRouter)
-app.use(plantRouter)
+const app = require('./app')
 
-app.get('/', (req, res) => {
-      res.send('Hello from our server!')
-})
-
+// rest of your code...
+const port = process.env.PORT || 8000
 app.listen(port, () => {
-      console.log('server listening on port 8000')
+//     console.log(process.env)
+    console.log('Running application')
 })
