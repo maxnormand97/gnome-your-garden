c6138b316e595bc69c5fa53f54b53d5a99b47212
Max Normand <maxnormand97@gmail.com>
2024-07-22 14:30:55 +0100

    cleanup for PR

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-console.log('process.env', process.env)
 require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
 const express = require('express');
 const app = express();
--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const app = require('./app')
-
-// rest of your code...
 const port = process.env.PORT || 8000
 app.listen(port, () => {
-//     console.log(process.env)
-    console.log('Running application')
+    console.log("You don't gnome what you don't gnome")
 })
